fb99fddae9167be987eb29062988f7dc05d08548
Supanccy <chenchaoyang2013@163.com>
2017-11-18 00:31:14 +0800

    添加固定位置的的搜索框组件

--- app/Wrap.js
+++ app/Wrap.js
@@ -7,20 +7,7 @@ import Middle from './middle/middle.js';
 import Bottom from './bottom/Bottom.js';
 import FixSearch from './top/fixSearch.js';
 
-
-
 class Wrap extends Component{
-
-    // //每隔3秒通过改变组件的status来更换背景图片
-    // componentDidMount(){
-    //     window.addEventListener('scroll',()=>{
-    //         var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    //         if(scrollTop > 120){
-    //             alert("开始进行");
-    //         }
-    //     })
-    // }
-
     render() {
         return (
             <div>
